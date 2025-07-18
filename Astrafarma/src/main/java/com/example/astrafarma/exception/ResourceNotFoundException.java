a41a9cc9a36e289e4b5eaffaabaf9315eee51c80
Josu31506 <josuehernandezyataco@gmail.com>
2025-07-17 20:31:45 -0500

    Delete Astrafarma directory

--- Astrafarma/src/main/java/com/example/astrafarma/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-// src/main/java/com/example/astrafarma/exception/ResourceNotFoundException.java
-package com.example.astrafarma.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
