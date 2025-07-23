03cad20801b7c97bfd07db5e5cba547eccb9f6d9
Josu31506 <josuehernandezyataco@gmail.com>
2025-07-23 13:31:48 -0500

    Return 404 for missing resources

--- src/main/java/com/example/astrafarma/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/astrafarma/exception/ResourceNotFoundException.java
@@ -1,7 +1,17 @@
 // src/main/java/com/example/astrafarma/exception/ResourceNotFoundException.java
 package com.example.astrafarma.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Exception thrown when a requested resource is not found.
+ * Annotated with {@link ResponseStatus} so Spring translates it to
+ * an HTTP 404 status instead of the default 500.
+ */
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
+
     public ResourceNotFoundException(String message) {
         super(message);
     }
