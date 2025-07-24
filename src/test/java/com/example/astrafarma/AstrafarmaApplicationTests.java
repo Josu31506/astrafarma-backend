8db0b680c4960044f22d0b61030edd5d31c161bf
Josu31506 <josuehernandezyataco@gmail.com>
2025-07-24 15:38:37 -0500

    Add files via upload

---
+++ src/test/java/com/example/astrafarma/AstrafarmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.astrafarma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstrafarmaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
